56cc8f4db96f8540b79e9d6b7d889c2f7b242687
rsally12 <romansally30@gmail.com>
2025-09-24 16:10:51 -0400

    chore: add db setup and Poll model schema

--- backend/db.py
+++ backend/db.py
@@ -1,3 +1,15 @@
 from sqlmodel import create_engine, SQLModel, Session
 
 engine = create_engine("sqlite:///./polls.db", connect_args={"check_same_thread": False})
+
+# create tables when app starts
+def init_db():
+    # Ensure models are registered with SQLModel's metadata before create_all
+    from models import Poll
+    SQLModel.metadata.create_all(engine) # looks at all models + builds the tables in database file
+    
+# add session maker
+def get_session():
+    return Session(engine)
+    
+    
